50639392c8d348de3786d7e408eddf9602f3aa64
kingkick <kingkick@163.com>
2024-06-07 15:27:40 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <body>{children}</body> */}
+      <body
+        className={`${inter.className} antialiased`}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
